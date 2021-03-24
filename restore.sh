3cd75cfe4fcae63f6047c304a8adf1e94d10c9f8
magcho <mail@magcho.com>
2021-03-24 23:57:07 +0900

    Update

--- restore.sh
+++ restore.sh
@@ -1,38 +1,32 @@
+#!/bin/zsh
 # mac restore shell script
 
-function installHomeBrew(){
-		# homebrewのインストール過程でxcode command line toolsもインストールされるため、xcode単体のインストールは不要
-		cd ~
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-}
-function restoreBrewTap(){
+restoreBrewTap () {
 		while read line
 		do
 				brew tap $line
 		done < ./lists/brewtap-list.txt
 }
-function restoreBrew () {
+restoreBrew () {
 		while read line
 		do
 				brew install $line
-		done < ./lists/brew-list.txt
+		done < ./lists/brew-list.
 }
-
-function restoreBrewCask () {
+restoreBrewCask () {
 		while read line
 		do
 				brew cask install $line
  		done < ./lists/brewcask-list.txt
 }
-function restoreAppstore () {
+restoreAppstore () {
 		while read line
 		do
 				mas install $(awk '{print $1}' <<<${line})
-
 		done < ./lists/mas-list.txt
 }
 
-function restoreMacOSConfig(){
+restoreMacOSConfig () {
 		# 隠しファイルを表示させる
 		defaults write com.apple.finder AppleShowAllFiles -bool true
 		# ライブラリーディレクトリを表示させる
@@ -60,16 +54,73 @@ function restoreMacOSConfig(){
 }
 
 
-function restoreDotfiles(){
- 		dotz restore
+
+
+
+main(){
+		echo '# Start restore'
+		
+		DOTZ_REPO_URL='https://github.com/magcho/.dotz'
+		DOTZ_ROOT_DIR=${DOTZ_ROOT:-"${HOME}/.dotz"}
+		
+		cd ${HOME}
+
+		if  [ ! type zplug > /dev/null 2>&1 ] && [ $SHELL = '/bin/zsh' ]; then
+				echo '# Install zplug'
+				curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+		fi
+
+		if [ "$(uname)" = 'Darwin' ]; then
+				if ! ( type brew > /dev/null 2>&1 ); then
+						echo '# Install homebrew'
+						/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+				fi
+				
+				if  [ ! -d "${HOME}/.dotz" ] || [ ! -n "${DOTZ_ROOT}" ]; then
+						echo '# Clone my dotfiles repo'
+						git clone ${DOTZ_REPO_URL} ${DOTZ_ROOT_DIR}
+				fi
+
+				
+				if [ type dotz > /dev/null 2>&1 ]; then
+						echo '# Install Dotz'
+						brew tap magcho/magcho
+						brew install dotz
+				fi
+				
+				if [ -d "${HOME}/.dotz" ] || [ -v DOTZ_ROOT ]; then
+						echo '# Restore dotfiles'
+						dotz restore
+				fi
+		fi
+
+		if [ -f "${HOME}/brew-ist.txt" ]; then
+				echo '# Restore brew comands'
+				restoreBrew
+		fi
+
+		if [ -f "${HOME}/brewcask-list.txt" ]; then
+				echo '# Restore brew cask apps'
+				restoreBrewCask
+		fi
+
+		if [ -f "${HOME}/mas-list.txt" ]; then
+				echo '# Restore mas apps'
+				restoreAppstore
+		fi
+
+		if [ ! -f '/Users/bigsur/Library/Fonts/SFMonoSquare-Regular.otf' ]; then
+				echo '# Generate programing font'
+				brew tap delphinus/sfmono-square
+				brew install sfmono-square
+				echo "# Generated $(brew --prefix sfmono-square)/share/fonts"
+				open "$(brew --prefix sfmono-square)/share/fonts"
+		fi
+
+		if [ "$(uname)" = 'Darwin' ]; then
+				echo '# Set for system prefernces'
+				restoreMacOSConfig
+		fi
 }
 
-
-
-
-# execute restore function
-# installHomebrew
-restoreBrewTap
-restoreBrew
-# restoreBrewCask
-# restoreAppstore
+main
